400e3a9fdb1fd7277bf3b60167e828dc980283f3
Kopsillom <kbgz0518>
2023-07-07 16:08:11 +0100

    Reverted Commit (Use this for backup)

--- app/src/main/java/com/example/finestapp/fournisseur/addFournisseur.java
+++ app/src/main/java/com/example/finestapp/fournisseur/addFournisseur.java
@@ -39,7 +39,7 @@ public class addFournisseur extends AppCompatActivity {
                 onBackPressed();
             }
         });
-
+//
         addbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
