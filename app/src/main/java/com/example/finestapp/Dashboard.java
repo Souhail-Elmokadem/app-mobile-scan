00e44fd5b754d1679cec26cb4a044f59a52148a4
Souhail El Mokadem <devsouhail1@gmail.com>
2023-07-09 23:21:10 +0100

    Edit Ui interface Dashboard add profile user

--- app/src/main/java/com/example/finestapp/Dashboard.java
+++ app/src/main/java/com/example/finestapp/Dashboard.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.finestapp.fournisseur.FournisseurList;
@@ -50,5 +52,6 @@ public class Dashboard extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), UserList.class));
             }
         });
+
     }
 }
