cad530f81a2e95290b4eb21efbc297a058cbf73f
Youssefhachim <hachim.youssef@gmail.com>
2023-07-21 17:16:51 +0100

    wip password and shown password

--- app/src/main/java/com/example/finestapp/Setting.java
+++ app/src/main/java/com/example/finestapp/Setting.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
